481b1f64cd78dd8ddb1cfea0dc4f226307b199e9
Isabel <83420259+isabel-mm@users.noreply.github.com>
2025-03-20 12:44:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -63,22 +63,23 @@ uploaded_files = st.file_uploader('Sube tus archivos .txt', type='txt', accept_m
 if uploaded_files:
     content_key = st.text_input('Nombre para el contenido (ej. "Texto")', value='content')
     label_key = st.text_input('Nombre para la etiqueta (ej. "Etiqueta")', value='label')
+    file_name = st.text_input('Nombre del archivo a descargar (sin extensión)', value='structured_data')
 
     structured_data = process_txt_files(uploaded_files)
 
     if st.button('Guardar como JSON'):
         json_path, structured_output = save_as_json(structured_data, content_key, label_key, 'output')
         st.success(f'Datos guardados como JSON en: {json_path}')
-        st.download_button(label='Descargar JSON', data=json.dumps(structured_output, indent=4), file_name='structured_data.json', mime='application/json')
+        st.download_button(label='Descargar JSON', data=json.dumps(structured_output, indent=4), file_name=f'{file_name}.json', mime='application/json')
 
     if st.button('Guardar como JSONL'):
         jsonl_path = save_as_jsonl(structured_data, content_key, label_key, 'output')
         st.success(f'Datos guardados como JSONL en: {jsonl_path}')
         jsonl_data = '\n'.join([json.dumps({content_key: item['content'], label_key: []}, ensure_ascii=False) for item in structured_data]).encode('utf-8')
-        st.download_button(label='Descargar JSONL', data=jsonl_data, file_name='structured_data.jsonl', mime='application/json')
+        st.download_button(label='Descargar JSONL', data=jsonl_data, file_name=f'{file_name}.jsonl', mime='application/json')
 
     if st.button('Guardar como CSV'):
         csv_path, df = save_as_csv(structured_data, content_key, label_key, 'output')
         st.success(f'Datos guardados como CSV en: {csv_path}')
         csv_data = df.to_csv(index=False).encode('utf-8')
-        st.download_button(label='Descargar CSV', data=csv_data, file_name='structured_data.csv', mime='text/csv')
+        st.download_button(label='Descargar CSV', data=csv_data, file_name=f'{file_name}.csv', mime='text/csv')
